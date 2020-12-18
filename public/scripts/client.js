71bc8899f36ec6a7a1133ac9e880b18cacee0cf1
Simon Huang <firebfm@gmail.com>
2020-12-18 00:25:30 +0000

    delete never used data variable

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,31 +5,6 @@
  */
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   // escaping user input to prevent XSS
   const escape =  function(str) {
     let div = document.createElement('div');
